8e8a3f29a90230138b99aabf93b4dedb93e33531
jjmayoragon <68620742+jjmayoragon@users.noreply.github.com>
2021-11-17 08:07:14 -0300

    DictionaryResources
    Impementation of dicionaryResources

--- SosMedicalWpf/SosMedicalWpf/obj/Debug/net5.0-windows/App.g.i.cs
+++ SosMedicalWpf/SosMedicalWpf/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace SosMedicalWpf {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/SosMedicalWpf;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SosMedicalWpf;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
